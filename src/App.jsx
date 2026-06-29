c4ad56c2a06310ee0d51bdb9ba2ab11408ab344f
Rishi Soni <rishisprogramming@gmail.com>
2026-06-29 22:29:54 +0500

    Added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import "./App.css";
 import { Container, Row } from "react-bootstrap";
 import Sidebar from "./Components/Sidebar";
 import MainArea from "./Components/MainArea";
+import CustomNavbar from "./Components/Navbar";
+
 
 const App = () => {
   return (
     <Container fluid>
+      <CustomNavbar/>
       <Row>
           <Sidebar/>
           <MainArea/>
