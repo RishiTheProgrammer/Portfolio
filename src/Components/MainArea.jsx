5e06155d1d6b8ca7093dc172339c360133df44dd
Rishi Soni <rishisprogramming@gmail.com>
2026-06-29 14:09:04 +0500

    Added overflow-y-scroll by adding vh-100

--- src/Components/MainArea.jsx
+++ src/Components/MainArea.jsx
@@ -2,8 +2,8 @@ import { Col } from 'react-bootstrap'
 
 const MainArea = () => {
   return (
-    <Col lg={10}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi dolorum minus beatae atque quod molestias quos dicta? Laudantium accusantium delectus sunt eveniet fuga reprehenderit dolor? Et quae dolorum eius culpa.
+    <Col lg={10} className='overflow-y-scroll vh-100'>
+        
     </Col>
   )
 }
