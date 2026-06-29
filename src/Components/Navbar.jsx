a89d311174991b5f8843b5829f47615ba91765f9
Rishi Soni <rishisprogramming@gmail.com>
2026-06-29 23:12:00 +0500

    Added components

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,29 +1,39 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const CustomNavbar = ()=> {
+    const tl = gsap.timeline()
+    useGSAP(()=>{
+        tl.from("#Navbar",{
+            opacity: 0,
+            y: -50,
+            duration: 1
+        });
+    })
   return (
-    <Navbar expand="lg" className="bg-body-tertiary d-lg-none fixed-top">
+    <Navbar expand="lg" className="bg-body-tertiary d-lg-none fixed-top" id="Navbar">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home" className='d-flex'><img src="src/Images/Logo.png" className="rounded-circle" height={40} width={40}/> 
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+          <Nav>
+            <Nav.Link href="#home"><i className="bi bi-house"></i> Home</Nav.Link>
+            <Nav.Link href="#link"><i className="bi bi-person"></i> About</Nav.Link>
+            <Nav.Link href="#link"><i className="bi bi-puzzle"></i> Skills</Nav.Link>
+            <Nav.Link href="#link"><i className="bi bi-folder"></i> Projects</Nav.Link>
+            <NavDropdown title="Conatct" id="basic-nav-dropdown" className="">
+              <NavDropdown.Item href="#action/3.1" className='w-100 d-flex align-items-center justify-content-between'><i className="bi bi-github fs-4"></i> GitHub</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2" className='w-100 d-flex align-items-center justify-content-between'>
+                <i className="bi bi-envelope fs-4"></i> Gmail
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3" className='w-100 d-flex align-items-center justify-content-between'><i className="bi bi-globe fs-4"></i> Rishi</NavDropdown.Item>
             </NavDropdown>
+
           </Nav>
         </Navbar.Collapse>
       </Container>
