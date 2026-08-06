177d0ef14659987ddaafaf55cfcc2a02b5b4a3fc
Rishi Soni <rishisprogramming@gmail.com>
2026-08-06 13:06:15 +0500

    Completed the About section

--- src/Sections/About.jsx
+++ src/Sections/About.jsx
@@ -17,18 +17,70 @@ const About = () => {
                 <Col md={6} className='text-start ps-xl-5'>
                     <Card className='bg-transparent border-0'>
                         <Card.Body>
-                            <Card.Title className='text-white display-4 fs-3 fw-semibold'><i class="bi bi-person text-primary"></i> About me</Card.Title>
+                            <Card.Title className='text-white display-4 fw-semibold'><i class="bi bi-person text-primary"></i> About me</Card.Title>
                             <Card.Text className='text-white mt-3'>I'm an 8th class student and a passionate frontend developer. I love to build functional and beautiful webpages. I always learn new technologies and improve my past skills.</Card.Text>
                         </Card.Body>
                     </Card>
                 </Col>
                 <Col md={6} className="text-lg-end text-center pe-xl-5">
-                    <Row>
-                        <Col>
-                            <Card className='bg-transparent border-2 border-primary'>
-                                <Card.Body>
-                                    <Card.Title className='text-white display-4 fs-3 fw-semibold'><i class="bi bi-code-slash text-primary"></i> Skills</Card.Title>
-                                    <Card.Text className='text-white mt-3'>HTML, CSS, JavaScript, ReactJS, Bootstrap, GitHub, Figma</Card.Text>
+                    <Row className='row-cols-2'>
+                        <Col xs={12} sm={6} className='border border-1 p-0'>
+                            <Card className='bg-transparent border-0 w-100'>
+                                <Card.Body className='text-center'>
+                                    <div className='d-flex'>
+                                        <div className='d-flex align-items-center justify-content-center text-center'>
+                                            <i className='bi bi-person display-5'></i>
+                                        </div>
+                                        <div className='d-flex align-items-start justify-content-center text-start flex-column ms-3 gap-2'>
+                                            <Card.Subtitle className='fs-5'>Name</Card.Subtitle>
+                                            <Card.Subtitle>Rishi</Card.Subtitle>
+                                        </div>
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col xs={12} sm={6} className='border border-1 p-0'>
+                            <Card className='bg-transparent border-0 w-100'>
+                                <Card.Body className='text-center'>
+                                    <div className='d-flex'>
+                                        <div className='d-flex align-items-center justify-content-center text-center'>
+                                            <i className='bi bi-mortarboard display-5'></i>
+                                        </div>
+                                        <div className='d-flex align-items-start justify-content-center text-start flex-column ms-3 gap-2'>
+                                            <Card.Subtitle className='fs-5'>Class</Card.Subtitle>
+                                            <Card.Subtitle>8th</Card.Subtitle>
+                                        </div>
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col xs={12} sm={6} className='border border-1 p-0'>
+                            <Card className='bg-transparent border-0 w-100'>
+                                <Card.Body className='text-center'>
+                                    <div className='d-flex'>
+                                        <div className='d-flex align-items-center justify-content-center text-center'>
+                                            <i className='bi bi-geo-alt-fill display-5'></i>
+                                        </div>
+                                        <div className='d-flex align-items-start justify-content-center text-start flex-column ms-3 gap-2'>
+                                            <Card.Subtitle className='fs-5'>Location</Card.Subtitle>
+                                            <Card.Subtitle>Pakistan</Card.Subtitle>
+                                        </div>
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col xs={12} sm={6} className='border border-1 p-0'>
+                            <Card className='bg-transparent border-0 w-100'>
+                                <Card.Body className='text-center'>
+                                    <div className='d-flex'>
+                                        <div className='d-flex align-items-center justify-content-center text-center'>
+                                            <i className='ri-target-fill display-5'></i>
+                                        </div>
+                                        <div className='d-flex align-items-start justify-content-center text-start flex-column ms-3 gap-2'>
+                                            <Card.Subtitle className='fs-5'>Focus</Card.Subtitle>
+                                            <Card.Subtitle>UI Dev.</Card.Subtitle>
+                                        </div>
+                                    </div>
                                 </Card.Body>
                             </Card>
                         </Col>
