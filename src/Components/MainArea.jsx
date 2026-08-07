26869523c55625abdc7a4d08134ef4af954debc3
Rishi Soni <rishisprogramming@gmail.com>
2026-08-07 21:01:49 +0500

    Imported Skill Section

--- src/Components/MainArea.jsx
+++ src/Components/MainArea.jsx
@@ -1,6 +1,7 @@
 import { Col } from 'react-bootstrap'
 import Home from '../Sections/Home.jsx'
 import About from '../Sections/About.jsx'
+import Skills from '../Sections/Skills.jsx'
 
 const MainArea = () => {
   
@@ -9,6 +10,7 @@ const MainArea = () => {
     <Col lg={10} className='overflow-y-scroll vh-100' id='mainArea' style={{scrollBehavior: "smooth"}}>
           <Home/>
           <About/>
+          <Skills/>
     </Col>
   )
 }
