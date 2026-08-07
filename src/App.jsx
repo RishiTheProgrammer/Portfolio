401fc30d679e6e6b53582cec1fa93c38ed23b794
Rishi Soni <rishisprogramming@gmail.com>
2026-08-07 17:14:43 +0500

    Added useEffect and GSAP file

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import { Container, Row } from "react-bootstrap";
 import Sidebar from "./Components/Sidebar";
 import MainArea from "./Components/MainArea";
 import CustomNavbar from "./Components/Navbar";
+import introTimeline from "./animations/introTimeline.jsx"
+import { useEffect } from 'react'
 
 
 const App = () => {
+  useEffect(() => {
+    introTimeline()
+  },[])
   return (
     <Container fluid className="App">
       <CustomNavbar/>
