d7d0476b03cf3dcc813f9cee4e6fe3618c4b377d
Rishi Soni <rishisprogramming@gmail.com>
2026-08-07 14:32:12 +0500

    Removed StrictMode to make animation happen

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -8,7 +8,6 @@ import "bootstrap-icons/font/bootstrap-icons.css"
 import "remixicon/fonts/remixicon.css"
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
     <App />
-  </StrictMode>,
 )
