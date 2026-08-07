fd1f54db180b3076d475f1a405ecccc244d8a5ca
Rishi Soni <rishisprogramming@gmail.com>
2026-08-07 17:13:37 +0500

    Removed useEffect and GSAP file

--- src/Components/MainArea.jsx
+++ src/Components/MainArea.jsx
@@ -1,13 +1,9 @@
 import { Col } from 'react-bootstrap'
 import Home from '../Sections/Home.jsx'
 import About from '../Sections/About.jsx'
-import introTimeline from '../animations/introTimeline.jsx'
-import { useEffect } from 'react'
 
 const MainArea = () => {
-  useEffect(() => {
-    introTimeline()
-  },[])
+  
 
   return (
     <Col lg={10} className='overflow-y-scroll vh-100' id='mainArea' style={{scrollBehavior: "smooth"}}>
