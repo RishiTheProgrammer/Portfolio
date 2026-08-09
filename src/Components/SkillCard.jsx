941d61ca7cad70d88b18a1fa32039cb865dc2cf6
Rishi Soni <rishisprogramming@gmail.com>
2026-08-09 11:54:31 +0500

    Increased Gap between icon and title

--- src/Components/SkillCard.jsx
+++ src/Components/SkillCard.jsx
@@ -1,19 +1,19 @@
 import { Col } from 'react-bootstrap'
 import Badge from 'react-bootstrap/Badge';
 
-const SkillCard = ({icon, title, level, text, badgeBg, iconClassName, xs, sm, md, lg, xl, CardClassName}) => {
+const SkillCard = ({icon, title, level, text, style, iconClassName, xs, sm, md, lg, xl, CardClassName}) => {
   return (
     <Col xs={xs} sm={sm} md={md} lg={lg} xl={xl} className={CardClassName}>
-        <div className='d-flex justify-content-between gap-3'>
+        <div className='d-flex justify-content-between gap-4'>
             <div className="d-flex align-items-start justify-content-center">
-                <span className={iconClassName}>{icon}</span>
+                <span className={iconClassName} style={style}>{icon}</span>
             </div>
             <div>
                 <h3>{title}</h3>
                 <p>{text}</p>
             </div>
         </div>
-        <Badge style={{backgroundColor: {badgeBg}}} className='fs-6'>{level}</Badge>
+        <Badge className='fs-6'>{level}</Badge>
     </Col>
   )
 }
