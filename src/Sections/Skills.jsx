a9dfe825d271cbd64d98d85f131abe93563472b3
Rishi Soni <rishisprogramming@gmail.com>
2026-08-10 14:43:35 +0500

    Editied some description and changed ScrollRevealing to AOS

--- src/Sections/Skills.jsx
+++ src/Sections/Skills.jsx
@@ -29,9 +29,9 @@ const Skills = () => {
     <Element name="skills">
       <section id="skills">
         <Container fluid style={{minHeight: "100vh"}} className='d-flex align-items-center flex-column'>
-            <h1 className="text-center display-5 fw-semibold mb-5 d-flex gap-2">Skills <span className='d-none d-lg-block'> That Power My Work</span></h1>
+            <h1 className="text-center display-5 fw-semibold mt-5 d-flex gap-2">Skills <span className='d-none d-lg-block'> That Power My Work</span></h1>
           <Row className='mt-5'>
-            <h3 className="text-primary mb-4"><RiCodeSSlashFill/> Fontend Development</h3>
+            <h3 className="text-primary mb-4"><RiCodeSSlashFill/> Frontend Development</h3>
             <SkillCard icon={<RiReactjsFill size={50} color="#4dd8eb"/>} title="React" level="Intermediate" text="Building interactive user interfaces with React." iconClassName="bg-primary-subtle rounded-3 p-2" xs={12} sm={6} xl={3} CardClassName='mt-5 mt-xl-0'/>
             <SkillCard icon={<SiJavascript size={58} color="#f0db4f" className='rounded-3'/>} title="JavaScript" level="Intermediate" text="Creating functional and dynamic web experiences." xs={12} sm={6} xl={3} CardClassName='mt-5 mt-xl-0'/>
             <SkillCard icon={<IoLogoHtml5 size={50} color="#fd7e14"/>} title="HTML5" level="Advanced" text="Structuring content for web page in the right way." iconClassName="bg-warning-subtle rounded-3 p-2" xs={12} sm={6} xl={3} CardClassName='mt-5 mt-xl-0'/>
@@ -39,16 +39,16 @@ const Skills = () => {
           </Row>
           <Row className='mt-5'>
             <h3 className="text-danger mb-4"><FaWandSparkles/> Animation</h3>
-            <SkillCard icon={<SiGsap size={50} color="#0ae448"/>} title="GSAP" level="Learning" text="Creating smooth and engaging animations." iconClassName="bg-success-subtle rounded-3 p-2" xs={12} sm={6} xl={3} CardClassName='mt-5 mt-xl-0'/>
+            <SkillCard icon={<SiGsap size={50} color="#0ae448"/>} title="GSAP" level="Learning" text="Creating smooth and creative animations." iconClassName="bg-success-subtle rounded-3 p-2" xs={12} sm={6} xl={3} CardClassName='mt-5 mt-xl-0'/>
             <SkillCard icon={<TbCircleLetterTFilled size={50} color="#520dc2"/>} title="Typed.js" level="Intermediate" text="Creating neat and beautiful typing animations." style={{backgroundColor: "#140330"}} iconClassName='rounded-3 p-2' xs={12} sm={6} xl={3} CardClassName='mt-5 mt-xl-0'/>
-            <SkillCard icon={<IoLogoCss3 size={50} color="#2061ef"/>} title="CSS3" level="Advanced" text="Animating modern websites with creativity." iconClassName="bg-primary-subtle rounded-3 p-2" xs={12} sm={6} xl={3} CardClassName='mt-5 mt-xl-0'/>
-            <SkillCard icon={<SiScrollreveal size={50} color="#1aa179"/>} title="ScrollReveal" level="Intermediate" text="Creating engaging and attractive scroll animations." iconClassName="rounded-3 p-2" style={{backgroundColor: "#06281e"}} xs={12} sm={6} xl={3} CardClassName='mt-5 mt-xl-0'/>
+            <SkillCard icon={<IoLogoCss3 size={50} color="#2061ef"/>} title="CSS3" level="Advanced" text="Animating modern websites with ease and creativity." iconClassName="bg-primary-subtle rounded-3 p-2" xs={12} sm={6} xl={3} CardClassName='mt-5 mt-xl-0'/>
+            <SkillCard icon={<SiScrollreveal size={50} color="#1aa179"/>} title="AOS" level="Intermediate" text="Creating engaging and attractive scroll animations." iconClassName="rounded-3 p-2" style={{backgroundColor: "#06281e"}} xs={12} sm={6} xl={3} CardClassName='mt-5 mt-xl-0'/>
           </Row>
           <Row className='mt-5'>
             <h3 className="text-success mb-4"><FaTools/> Tools and Others</h3>
-            <SkillCard icon={<FaGitAlt size={50} color="#f34f29"/>} title="Git" level="Advanced" text="Tool I use to commit my projects." iconClassName="bg-danger-subtle rounded-3 p-2" xs={12} sm={6} xl={3} CardClassName='mt-5 mt-xl-0'/>
+            <SkillCard icon={<FaGitAlt size={50} color="#f34f29"/>} title="Git" level="Advanced" text="Tool I use to commit my projects to GitHub." iconClassName="bg-danger-subtle rounded-3 p-2" xs={12} sm={6} xl={3} CardClassName='mt-5 mt-xl-0'/>
             <SkillCard icon={<FaGithub size={50} color="#fafafa"/>} title="GitHub" level="Intermediate" text="Tool I use to manage my code repositories." iconClassName='bg-dark-subtle rounded-3 p-2' xs={12} sm={6} xl={3} CardClassName='mt-5 mt-xl-0'/>
-            <SkillCard icon={<VscVscode size={50} color="#0078d4"/>} title="VS Code" level="Advanced" text="Tool I use to create websites  with ease and creativity." iconClassName="bg-primary-subtle rounded-3 p-2" xs={12} sm={6} xl={3} CardClassName='mt-5 mt-xl-0'/>
+            <SkillCard icon={<VscVscode size={50} color="#0078d4"/>} title="VS Code" level="Advanced" text="Tool I use to create websites with ease and creativity." iconClassName="bg-primary-subtle rounded-3 p-2" xs={12} sm={6} xl={3} CardClassName='mt-5 mt-xl-0'/>
             <SkillCard icon={<SiVite size={50} color="#9053ff"/>} title="Vite" level="Intermediate" text="Fast build tool for modern web development." iconClassName="rounded-3 p-2" style={{backgroundColor: "#160d27"}} xs={12} sm={6} xl={3} CardClassName='mt-5 mt-xl-0'/>
           </Row>
         </Container>
