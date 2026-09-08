f8c0191603fc9fa919c198801587506ca145fb60
Rishi Soni <rishisprogramming@gmail.com>
2026-09-08 21:20:36 +0500

    Removed the animations for now

--- src/animations/introTimeline.jsx
+++ src/animations/introTimeline.jsx
@@ -6,37 +6,7 @@ const introTimeline = () => {
     const tl = gsap.timeline({
         timeScale: 2
       });
-        tl.from(".sidebar", {
-          x: "-100%",
-          duration: .5,
-          delay: 1
-        });
-    
-        tl.from(".img-logo", {
-          y: 20,
-          opacity: 0,
-          duration: .5
-        });
-    
-        tl.from(".logoName .a", {
-          opacity: 0,
-          duration: .5,
-          stagger: .2,
-        });
-    
-        tl.from(".nav-link", {
-          opacity: 0,
-          x: 20,
-          duration: .5,
-          stagger: .2,
-        });
-    
-        tl.from(".logoBrand", {
-          opacity: 0,
-          y: 20,
-          duration: .5,
-          stagger: .2,
-        });
+        
     }
 
 export default introTimeline
