586ad0b9ad6a3e42199f41f6406ab0595e27f1f0
Rishi Soni <rishisprogramming@gmail.com>
2026-09-08 21:19:04 +0500

    Removed class

--- src/Components/MainArea.jsx
+++ src/Components/MainArea.jsx
@@ -7,7 +7,7 @@ const MainArea = () => {
   
 
   return (
-    <Col lg={10} className='overflow-y-scroll vh-100' id='mainArea' style={{scrollBehavior: "smooth"}}>
+    <Col id='mainArea' style={{scrollBehavior: "smooth"}}>
           <Home/>
           <About/>
           <Skills/>
