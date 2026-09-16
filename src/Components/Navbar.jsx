fb44616334ff97605e235c8bf1fbf2d1321d655e
Rishi Soni <rishisprogramming@gmail.com>
2026-09-16 17:14:01 +0500

    Used NavLink from react router instead of Link from react scroll. And made a floating effect on Navbar element

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,68 +2,51 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import './Navbar.css'
-import { Link } from "react-scroll";
+import { NavLink } from "react-router-dom";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger, useGSAP);
 
 const CustomNavbar = () => {
+  const navClass = ({ isActive }) => isActive ? "nav-link active" : "nav-link";
+
   return (
-    <Navbar expand="lg" className="fixed-top" id="navbar">
+    <Navbar expand="lg" className="fixed-top rounded-4 m-3" id="navbar">
       <Container>
         <Navbar.Brand href="#home" className="mt-2">
-          <h2>RISHI</h2>
+          <h2><i className='bi bi-lightning-charge-fill'></i> RISHI</h2>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="d-flex align-items-center gap-3 ms-auto">
-            <Link
-              to="home"
-              spy={true}
-              smooth={false}
-              offset={-56}
-              activeClass="active"
-              className="nav-link"
+          <Nav className="d-flex align-items-center justify-content-evenly gap-lg-3 ms-lg-auto flex-md-row">
+            <NavLink
+              to="/" className={navClass} end
             >
               <i className="bi bi-house-fill me-1"></i> Home
-            </Link>
-            <Link
-              to="about"
-              spy={true}
-              smooth={false}
-              offset={-56}
-              activeClass="active"
-              className="nav-link"
+            </NavLink>
+            <NavLink
+              to="/about" className={navClass}
             >
               <i className="bi bi-person-fill me-1"></i> About
-            </Link>
-            <Link
-              to="skills"
-              spy={true}
-              smooth={false}
-              offset={-56}
-              activeClass="active"
-              className="nav-link"
+            </NavLink>
+            <NavLink
+              to="/skills" className={navClass}
             >
               <i className="bi bi-mortarboard-fill me-1"></i> Skills
-            </Link>
-            <Link
-              to="projects"
-              spy={true}
-              smooth={false}
-              offset={-56}
-              activeClass="active"
-              className="nav-link"
+            </NavLink>
+            <NavLink
+              to="/projects" className={navClass}
             >
               <i className="bi bi-briefcase-fill me-1"></i> Projects
-            </Link>
-            <Link
-              to="contact"
-              spy={true}
-              smooth={false}
-              offset={-56}
-              activeClass="active"
-              className="nav-link"
+            </NavLink>
+            <NavLink
+              to="/contact" className={navClass}
             >
               <i className="bi bi-telephone-fill me-1"></i> Contact
-            </Link>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
