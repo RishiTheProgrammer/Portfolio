6919863c3169667e5e65de2ae1f6aaac0e23ea79
Rishi Soni <rishisprogramming@gmail.com>
2026-09-16 17:06:31 +0500

    Update portfolio structure and dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
 import "./App.css"; 
 import { Container, Row } from "react-bootstrap";
 import MainArea from "./Components/MainArea";
-import CustomNavbar from "./Components/Navbar";
-import introTimeline from "./animations/introTimeline.jsx"
 import { useEffect } from 'react'
+import gsap from 'gsap'
 
 
 const App = () => {
   useEffect(() => {
-    introTimeline()
+    const cursor = document.querySelector(".custom-cursor");
+
+  const xTo = gsap.quickTo(cursor, "x", {
+    duration: 0.15
+  });
+
+  const yTo = gsap.quickTo(cursor, "y", {
+    duration: 0.15
+  });
+
+  const moveCursor = (e) => {
+    xTo(e.clientX);
+    yTo(e.clientY);
+  };
+
+  window.addEventListener("mousemove", moveCursor);
+
+  return () => {
+    window.removeEventListener("mousemove", moveCursor);
+  };
   },[])
   return (
     <Container fluid className="App">
-      <CustomNavbar/>
+      <div className="custom-cursor d-none d-lg-block"></div>
       <Row>
           <MainArea/>
       </Row>
--- src/Components/MainArea.jsx
+++ src/Components/MainArea.jsx
@@ -2,15 +2,18 @@ import { Col } from 'react-bootstrap'
 import Home from '../Sections/Home.jsx'
 import About from '../Sections/About.jsx'
 import Skills from '../Sections/Skills.jsx'
+import { Routes, Route } from "react-router-dom";
 
 const MainArea = () => {
   
 
   return (
     <Col id='mainArea' style={{scrollBehavior: "smooth"}}>
-          <Home/>
-          <About/>
-          <Skills/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+      </Routes>
     </Col>
   )
 }
