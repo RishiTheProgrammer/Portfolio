1252b8ebbe2e8972848513fd70796cfe38bf04f3
Rishi Soni <rishisprogramming@gmail.com>
2026-09-16 17:33:27 +0500

    Covered 'App' wiht BrowserRouter by react router

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,10 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.min.js"
 import "bootstrap-icons/font/bootstrap-icons.css"
 import "remixicon/fonts/remixicon.css"
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  
-    <App />
+  <BrowserRouter>
+  <App />
+</BrowserRouter>
 )
