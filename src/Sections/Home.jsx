ef842282f6212bdeacf7a8a6e00fb8c88623760e
Rishi Soni <rishisprogramming@gmail.com>
2026-09-17 17:39:57 +0500

    Added content and Aurora background from React Bits

--- src/Sections/Home.jsx
+++ src/Sections/Home.jsx
@@ -1,22 +1,16 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "@gsap/react";
-import { Container, Row, Col, Card } from "react-bootstrap";
+import { Row, Col, Card } from "react-bootstrap";
 import { useGSAP } from "@gsap/react";
 import "./Home.css";
 // import hero from "../assets/HeroIMG.png"
-import IntroTimeline from "../animations/introTimeline.jsx";
+import homeAnimation from "../animations/homeAnimation.jsx";
 import CustomNavbar from "../Components/Navbar";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useRef } from "react";
-import { IoLogoJavascript } from "react-icons/io5";
-import { SiHtml5 } from "react-icons/si";
-import { IoLogoCss3 } from "react-icons/io";
-import { FaReact } from "react-icons/fa";
-import { BsBootstrapFill } from "react-icons/bs";
-import { SiVite } from "react-icons/si";
-import { SiGsap } from "react-icons/si";
-import { FaGithub } from "react-icons/fa6";
+import AuroraBG from "../Components/AuroraBG";
+import SpotlightCard from "../Components/SpotlightCard";
 
 gsap.registerPlugin(ScrollTrigger, useGSAP);
 
@@ -25,84 +19,49 @@ const Home = () => {
   const heroSubTitleRef = useRef();
   const heroTextRef = useRef();
   const ctaBtnRef = useRef();
-  const navigate = useNavigate();
   useGSAP(() => {
-    IntroTimeline({ heroTitleRef, heroSubTitleRef, heroTextRef, ctaBtnRef });
+    homeAnimation({ heroTitleRef, heroSubTitleRef, heroTextRef, ctaBtnRef });
   });
   return (
     <section id="home">
-      <Container fluid>
-        <CustomNavbar />
-        <Row className="d-flex align-items-center min-vh-100">
-          <Col>
-        <div className="cloud-space">
-            <div className="cloud-particles">
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-</div>
-<div className="cloud-background-text">
-    DEVELOPER
-</div>
-          <div className="cloud-core">
-            <span>R</span>
-          </div>
-          <div className="cloud-icon icon-html">
-            <SiHtml5 />
-          </div>
-          <div className="cloud-icon icon-css">
-            <IoLogoCss3 />
-          </div>
-          <div className="cloud-icon icon-js">
-            <IoLogoJavascript />
-          </div>
-          <div className="cloud-icon icon-bootstrap">
-            <BsBootstrapFill />
-          </div>
-          <div className="cloud-icon icon-react">
-            <FaReact />
-          </div>
-          <div className="cloud-icon icon-vite">
-            <SiVite />
-          </div>
-          <div className="cloud-icon icon-gsap">
-            <SiGsap />
-          </div>
-          <div className="cloud-icon icon-github">
-            <FaGithub />
-          </div>
+      <div>
+        <div className='aurora-background'>
+          <AuroraBG />
         </div>
+        <CustomNavbar />
+        <Row>
+          <Col className='min-vh-100 d-flex align-items-center justify-content-center'>
+            <div className='hero-content'>
+                <h1 className="hero-title text-uppercase text-center">I don't only create websites</h1>
+                <h3 className='hero-subtitle text-center text-uppercase'>I create worlds</h3>
+                <h5 className="text-center">Frontend Developer | UI/UX Designer | Motion Designer</h5>
+                <div className="mx-auto d-flex justify-content-center gap-5">
+                <Link to="/about" className="btn btn-light btn-lg mt-3">Learn More</Link>
+                <Link to="/skills" className="btn btn-outline-light btn-lg mt-3">See Skills</Link>
+                </div>
+            </div>
           </Col>
         </Row>
-        <Row className="d-flex align-items-center min-vh-100 g-3">
+        <Row className="align-items-center min-vh-100 g-3">
           <div className="mb-5">
             <h1 className="text-center display-1 fw-semibold">What I build?</h1>
             <h3 className="text-center display-4 fw-semibold">
               Interfaces that feels alive.
             </h3>
           </div>
-          <Col lg={8}>
-            <Card className="bg-transparent border-0 h-100">
-              <Card.Body style={{ minHeight: "400px" }} className="text-center">
-                <Card.Title className="text-white display-5 fw-semibold">
-                  React Interfaces
-                </Card.Title>
-                <Card.Text className="text-white display-6 fw-semibold">
-                  React + UI + Components
-                </Card.Text>
-              </Card.Body>
-            </Card>
+          <Col lg={6}>
+            <SpotlightCard>
+                <Card.Body className='p-4 text-center'>
+                    <Card.Title className="text-white display-5 fw-semibold">
+                      React Interfaces
+                    </Card.Title>
+                    <Card.Text className="text-white display-6 fw-semibold mt-3">
+                      React + UI + Components
+                    </Card.Text>
+                </Card.Body>
+            </SpotlightCard>
           </Col>
-          <Col lg={4}>
+          <Col lg={6}>
             <Row className="g-3">
               <Col>
                 <Card className="bg-transparent border-0">
@@ -125,7 +84,7 @@ const Home = () => {
             </Row>
           </Col>
         </Row>
-      </Container>
+      </div>
     </section>
   );
 };
