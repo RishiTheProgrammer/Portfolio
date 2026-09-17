0d3715dcf0a65de6b1412beb28e1b1d007c6d89f
Rishi Soni <rishisprogramming@gmail.com>
2026-09-17 17:58:56 +0500

    Made the button responsive on mobile

--- src/Sections/Home.jsx
+++ src/Sections/Home.jsx
@@ -35,7 +35,7 @@ const Home = () => {
                 <h1 className="hero-title text-uppercase text-center">I don't only create websites</h1>
                 <h3 className='hero-subtitle text-center text-uppercase d-none d-sm-block'>I create worlds</h3>
                 <h5 className="text-center">Frontend Developer | UI/UX Designer | Motion Designer</h5>
-                <div className="mx-auto d-flex justify-content-center gap-5">
+                <div className="mx-auto d-flex justify-content-center gap-lg-5 flex-column flex-lg-row px-5 px-lg-0">
                 <Link to="/about" className="btn btn-light btn-lg mt-3">Learn More</Link>
                 <Link to="/skills" className="btn btn-outline-light btn-lg mt-3">See Skills</Link>
                 </div>
