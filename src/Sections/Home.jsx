4e90b2807f8149b8625800a5048dd18d2d6876f1
Rishi Soni <rishisprogramming@gmail.com>
2026-09-18 21:16:46 +0500

    Changed the sentence responsive to title from main quotation

--- src/Sections/Home.jsx
+++ src/Sections/Home.jsx
@@ -35,8 +35,8 @@ const Home = () => {
         <Row>
           <Col className='min-vh-100 d-flex align-items-center justify-content-center pt-3 pt-sm-0'>
             <div className='hero-content'>
-                <h1 className="hero-title text-uppercase text-center">I don't only create websites</h1>
-                <h3 className='hero-subtitle text-center text-uppercase d-none d-sm-block'>I create worlds</h3>
+                <h1 className="hero-title text-uppercase text-center d-none d-sm-block">I don't only create websites</h1>
+                <h3 className='hero-subtitle text-center text-uppercase'>I create worlds</h3>
                 <h5 className="text-center">Frontend Developer | UI/UX Designer | Motion Designer</h5>
                 <div className="mx-auto d-flex justify-content-center gap-lg-5 flex-column flex-lg-row px-5 px-lg-0">
                 <Link to="/about" className="btn btn-light btn-lg mt-3">Learn More</Link>
