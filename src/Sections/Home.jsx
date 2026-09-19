f7d70334ff6dfee6d5bd9a9552a46d9fcbf93897
Rishi Soni <rishisprogramming@gmail.com>
2026-09-19 16:45:15 +0500

    Called font-ilatianno to make class based font

--- src/Sections/Home.jsx
+++ src/Sections/Home.jsx
@@ -66,7 +66,7 @@ const Home = () => {
           <Col className='min-vh-100 d-flex align-items-center justify-content-center pt-3 pt-sm-0'>
             <div className='hero-content'>
                 <h1 className="hero-title text-uppercase text-center d-none d-sm-block">I don't only create websites</h1>
-                <h3 className='hero-subtitle text-center text-uppercase'>I create worlds</h3>
+                <h3 className='hero-subtitle text-center text-uppercase font-italianno'>I create worlds</h3>
                 <h5 className="text-center">Frontend Developer | UI/UX Designer | Motion Designer</h5>
                 <div className="mx-auto d-flex justify-content-center gap-sm-5 flex-column flex-sm-row px-5 px-lg-0">
                 <Link to="/about" className="btn btn-light btn-lg mt-3">Learn More</Link>
