ca2a29b92e95d92175057e7629c70a3e77e313dc
Rishi Soni <rishisprogramming@gmail.com>
2026-09-19 10:52:43 +0500

    Resolved the row mechanism for sm or higher devices

--- src/Sections/Home.jsx
+++ src/Sections/Home.jsx
@@ -46,7 +46,7 @@ const Home = () => {
                 <h1 className="hero-title text-uppercase text-center d-none d-sm-block">I don't only create websites</h1>
                 <h3 className='hero-subtitle text-center text-uppercase'>I create worlds</h3>
                 <h5 className="text-center">Frontend Developer | UI/UX Designer | Motion Designer</h5>
-                <div className="mx-auto d-flex justify-content-center gap-lg-5 flex-column flex-sm-row px-5 px-lg-0">
+                <div className="mx-auto d-flex justify-content-center gap-sm-5 flex-column flex-sm-row px-5 px-lg-0">
                 <Link to="/about" className="btn btn-light btn-lg mt-3">Learn More</Link>
                 <Link to="/skills" className="btn btn-outline-light btn-lg mt-3">See Skills</Link>
                 </div>
