0ec34ed88b836d1f18eca3204026fa66b2bb0c57
Rishi Soni <rishisprogramming@gmail.com>
2026-09-20 16:40:39 +0500

    Removed the mechanism which made the background responsive

--- src/Sections/Home.jsx
+++ src/Sections/Home.jsx
@@ -21,16 +21,6 @@ const Home = () => {
   useGSAP(() => {
     homeAnimation({ heroTitleRef, heroSubTitleRef, });
   });
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-  useEffect(()=>{
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
   const statCard = [
     {
       id: 1,
